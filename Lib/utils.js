7e9e0c2b1abb43285aac72486d2ff8c9a5201bd5
Adam Tal <adamtal3@gmail.com>
2017-07-17 22:45:59 +0300

    Run exec instead of spawn on OSX so open command will work #15

--- Lib/utils.js
+++ Lib/utils.js
@@ -1,20 +1,20 @@
-const fs = require( "fs-extra" ),
-      { spawn } = require( "child_process" );
+const fs = require("fs-extra"),
+    { spawn, exec } = require("child_process"),
+    { IS_OSX } = require("./env");
 /**
-  * Remove trailing slash
-  * @param {string} dir
-  * @returns {string}
-  */
-function rtrim( dir )
-{
-  return dir.replace( /\/$/, "" );
+ * Remove trailing slash
+ * @param {string} dir
+ * @returns {string}
+ */
+function rtrim(dir) {
+    return dir.replace(/\/$/, "");
 }
 /**
  * Remove a directory with content
  * @param {string} dir
  */
-async function remove( dir ){
-  fs.removeSync( dir );
+async function remove(dir) {
+    fs.removeSync(dir);
 }
 
 /**
@@ -24,17 +24,17 @@ async function remove( dir ){
  * @param {FileDescriptor} log
  * @returns {Promise}
  */
-async function copy( from, to, log ){
-  return new Promise(( resolve, reject ) => {
-    fs.writeSync( log, `copy "${from}" "${to}"\n`, "utf-8" );
-    fs.copy( from, to, ( err ) => {
-      if ( err ) {
-        fs.writeSync( log, `ERROR: ${err}\n`, "utf-8" );
-        return reject( err );
-      }
-      resolve();
+async function copy(from, to, log) {
+    return new Promise((resolve, reject) => {
+        fs.writeSync(log, `copy "${from}" "${to}"\n`, "utf-8");
+        fs.copy(from, to, (err) => {
+            if (err) {
+                fs.writeSync(log, `ERROR: ${err}\n`, "utf-8");
+                return reject(err);
+            }
+            resolve();
+        });
     });
-  });
 }
 
 /**
@@ -45,32 +45,50 @@ async function copy( from, to, log ){
  * @param {string} logPath
  * @returns {Promise}
  */
-async function launch( runnerPath, argv, cwd, logPath ){
-   return new Promise(( resolve, reject ) => {
-      const log = fs.openSync( logPath, "a" ),
+async function launch(runnerPath, argv, cwd, logPath) {
+    return new Promise((resolve, reject) => {
+        const log = fs.openSync(logPath, "a");
 
-      child = spawn( runnerPath, argv, {
-         timeout: 4000,
-         detached: true,
-         cwd
-       });
+        if (IS_OSX) {
+            exec(`${runnerPath} ${argv.join(' ')}`, (error, stdout, stderr) => {
+                if (error) {
+                    fs.writeSync(log, ["ERROR:", error, "\r\n"].join(" "), "utf-8");
+                    reject(log);
+                }
 
-      child.stdout.on( "data", ( data ) => {
-         fs.writeSync( log, `${data}`, "utf-8" );
-      });
+                if (stdout) {
+                    fs.writeSync(log, `${stdout}`, "utf-8");
+                }
 
-      child.stderr.on( "data", ( data ) => {
-        fs.writeSync( log, `ERROR: ${data}`, "utf-8" );
-      });
+                if (stderr) {
+                    fs.writeSync(log, `ERROR: ${stderr}`, "utf-8");
+                }
+            });
+        } else {
+            const child = spawn(runnerPath, argv, {
+                timeout: 4000,
+                detached: true,
+                cwd
+            });
 
-      child.on( "error", ( e ) => {
-        fs.writeSync( log, [ "ERROR:", e, "\r\n" ].join( " " ), "utf-8" );
-        reject( log );
-      });
+            child.stdout.on("data", (data) => {
+                fs.writeSync(log, `${data}`, "utf-8");
+            });
 
-       child.unref();
-       setTimeout( resolve, 500 );
-   });
+            child.stderr.on("data", (data) => {
+                fs.writeSync(log, `ERROR: ${data}`, "utf-8");
+            });
+
+            child.on("error", (e) => {
+                fs.writeSync(log, ["ERROR:", e, "\r\n"].join(" "), "utf-8");
+                reject(log);
+            });
+
+            child.unref();
+        }
+
+        setTimeout(resolve, 500);
+    });
 }
 
 exports.launch = launch;
