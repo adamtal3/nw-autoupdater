aa510a5d22732e61c5e6bcdf07978cfeff05d805
Adam Tal <adamtal3@gmail.com>
2017-07-18 00:18:48 +0300

    Removed -a from open command because it's blocked

--- Lib/Strategy/AppSwap.js
+++ Lib/Strategy/AppSwap.js
@@ -1,79 +1,78 @@
-const { join  } = require( "path" ),
-      fs = require( "fs-extra" ),
-      { launch, copy } = require( "../utils" ),
-      { swapFactory, IS_OSX } = require( "../env" );
+const { join } = require("path"),
+    fs = require("fs-extra"), { launch, copy } = require("../utils"), { swapFactory, IS_OSX } = require("../env");
 
 
-  /**
-   * Restart and launch detached swap
-   * @returns {Promise}
-   */
-  async function restartToSwap(){
+/**
+ * Restart and launch detached swap
+ * @returns {Promise}
+ */
+async function restartToSwap() {
     const { execDir, executable, updateDir, backupDir, logPath } = this.options,
-          tpmUserData = join( nw.App.dataPath, "swap" ),
-          app = join( updateDir, executable ),
-          args = [ `--user-data-dir=${tpmUserData}`,
-            `--swap=${execDir}`, `--bak-dir=${backupDir}` ];
+        tpmUserData = join(nw.App.dataPath, "swap"),
+        app = join(updateDir, executable),
+        args = [`--user-data-dir=${tpmUserData}`,
+            `--swap=${execDir}`, `--bak-dir=${backupDir}`
+        ];
 
-    if ( IS_OSX ) {
-      await launch( "open", [ "-a", app, "--args", ...args ], updateDir, logPath );
+    if (IS_OSX) {
+        await launch("open", [app, "--args", ...args], updateDir, logPath);
     } else {
-      await launch( app, args, updateDir, logPath );
+        await launch(app, args, updateDir, logPath);
     }
     nw.App.quit();
-  }
+}
 
-  function getBakDirFromArgv( argv ){
-    const raw = argv.find( arg => arg.startsWith( "--bak-dir=" ) );
-    if ( !raw ) {
-      return false;
+function getBakDirFromArgv(argv) {
+    const raw = argv.find(arg => arg.startsWith("--bak-dir="));
+    if (!raw) {
+        return false;
     }
-    return raw.substr( 10 );
-  }
+    return raw.substr(10);
+}
 
-  /**
-   * Is it a swap request
-   * @returns {Boolean}
-   */
-  function isSwapRequest(){
-    const raw = this.argv.find( arg => arg.startsWith( "--swap=" ) );
-    if ( !raw ) {
-      return false;
+/**
+ * Is it a swap request
+ * @returns {Boolean}
+ */
+function isSwapRequest() {
+    const raw = this.argv.find(arg => arg.startsWith("--swap="));
+    if (!raw) {
+        return false;
     }
 
-    this.options.execDir = raw.substr( 7 );
-    this.options.backupDir = getBakDirFromArgv( this.argv ) || this.options.backupDir;
+    this.options.execDir = raw.substr(7);
+    this.options.backupDir = getBakDirFromArgv(this.argv) || this.options.backupDir;
     return true;
-  }
-  /**
-   * Do swap
-   */
-  async function swap(){
+}
+/**
+ * Do swap
+ */
+async function swap() {
     const { executable, backupDir, execDir, updateDir, logPath } = this.options,
-          log = fs.openSync( logPath, "a" );
-    if ( IS_OSX ) {
-        await copy( join( execDir, executable ), backupDir, log );
-        await copy( updateDir, execDir, log );
+        log = fs.openSync(logPath, "a");
+    if (IS_OSX) {
+        await copy(join(execDir, executable), backupDir, log);
+        await copy(updateDir, execDir, log);
     } else {
-        await copy( execDir, backupDir, log );
-        await copy( updateDir, execDir, log );
+        await copy(execDir, backupDir, log);
+        await copy(updateDir, execDir, log);
     }
-  }
-  /**
-   * REstart after swap
-   * @returns {Promise}
-   */
-  async function restart(){
+}
+/**
+ * REstart after swap
+ * @returns {Promise}
+ */
+async function restart() {
     const { execDir, executable, updateDir, logPath } = this.options,
-          app = join( execDir, executable );
+        app = join(execDir, executable);
 
-     if ( IS_OSX ) {
-      await launch( "open", [ "-a", app, "--args" ], execDir, logPath );
+    if (IS_OSX) {
+        await launch("open", [app, "--args"], execDir, logPath);
     } else {
-      await launch( app, [], execDir, logPath );
+        await launch(app, [], execDir, logPath);
     }
     nw.App.quit();
-  }
+}
 
 
 exports.restartToSwap = restartToSwap;
